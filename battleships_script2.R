e3ee9470c959be6e40dbe411352fbea0c518dbc2
MichalStryjek <98279476+MichalStryjek@users.noreply.github.com>
2022-06-13 14:11:34 +0200

    UPDATED game AND shoot

--- battleships_script2.R
+++ battleships_script2.R
@@ -5,7 +5,7 @@ library(pryr)
 #Create new environment that will be used to contain objects modified by functions
 
 en<-new.env()
-
+ai<-new.env()
 
 ### Create clear board function ###
 
@@ -173,7 +173,7 @@ place_ship_on_board<-function(obj_board,obj_ship,vect_spot,vert=TRUE){
 ## This function will populate the board based on semi-random algorithm  
 
 
-populate_board<-function()
+populate_board<-function(qn)
 {
   
   
@@ -205,7 +205,7 @@ populate_board<-function()
   ship23<-create_ship(2,23)
   
   ship31<-create_ship(3,31)
-  ship32<-create_ship(3,31)
+  ship32<-create_ship(3,32)
   
   ship41<-create_ship(4,41)
   ## safety function iterator
@@ -219,7 +219,7 @@ populate_board<-function()
   ## Group ships into list
   
   ship_list<-list("ship41"=ship41,"ship32"=ship32,"ship31"=ship31,"ship23"=ship23,"ship22"=ship22,"ship21"=ship21,"ship14"=ship14,"ship13"=ship13,"ship12"=ship12,"ship11"=ship11)
-  en$ship_list<-list(ship41,ship32,ship31,ship23,ship22,ship21,ship14,ship13,ship12,ship11)
+  #en$ship_list<-list(ship41,ship32,ship31,ship23,ship22,ship21,ship14,ship13,ship12,ship11)
   ## Possible vertical/horizontal values
   vert_list<-c(TRUE,FALSE)
   
@@ -272,7 +272,7 @@ populate_board<-function()
      #message(names(ship_list)[i])
      #message(placing[[2]])
 
-     assign(paste0(names(ship_list)[i]),placing[[2]],envir = en)
+     assign(paste0(names(ship_list)[i]),placing[[2]],envir = qn)
      i=i+1
   }
   
@@ -290,66 +290,149 @@ populate_board<-function()
 # This function will check if the field can be shot and shoot it, if possible
 shoot<-function(obj_board,vect_spot)
 {
-  if (!obj_board[vect_spot[1],vect_spot[2],2] %in% c(2,3,4)){
+      if (!obj_board[vect_spot[1],vect_spot[2],2] %in% c(2,3,4)){
     
-    if (obj_board[vect_spot[1],vect_spot[2],2] %in% c(6,0)){
-      return(4)
-    }
+            if (obj_board[vect_spot[1],vect_spot[2],2] %in% c(6,0)){
+                  return("MISS")
+            }
     
-    if (obj_board[vect_spot[1],vect_spot[2],2]==1){
-      
-      
-      return(2)
-      
-    }
-    
-      return("MISS")
-    
-  }
+            if (obj_board[vect_spot[1],vect_spot[2],2]==1){
+                  return("HIT")
+            }
+      }
   
   
-  return("NA")
+        return("NA")
   
 }
 
 
 
-
+where("id"==14)
 
 
 
 game<-function(board1,board2){
   
-  turn_ind=1
   
-  player=list(board1,"filler",board2)
+      turn_ind=1
   
-  while ((sum(board1[,,2]==4)!=20)&& (sum(board2[,,2]==4)!=20)){
-    
-    
-    coords_y<-readline(prompt="Insert Coordinate y: ")
-    coords_x<-readline(prompt="Insert Coordinate x: ")
-    
-    coords_x<-as.integer(coords_x)
-    coords_y<-as.integer(coords_y)
-    
-    coords=c(coords_y,coords_x)
-    
-    result<-shoot(player[[2+turn_ind]],coords)
-    message(result)
-  }
+      coor_list<-list(c(1,1),c(1,2),c(1,3),c(1,4),c(1,5),c(1,6),c(1,7),c(1,8),c(1,9),c(1,10),c(2,1),c(2,2),c(2,3),c(2,4),c(2,5),c(2,6),c(2,7),c(2,8),c(2,9),c(2,10),c(3,1),c(3,2),c(3,3),c(3,4),c(3,5),c(3,6),c(3,7),c(3,8),c(3,9),c(3,10),c(4,1),c(4,2),c(4,3),c(4,4),c(4,5),c(4,6),c(4,7),c(4,8),c(4,9),c(4,10),c(5,1),c(5,2),c(5,3),c(5,4),c(5,5),c(5,6),c(5,7),c(5,8),c(5,9),c(5,10),c(6,1),c(6,2),c(6,3),c(6,4),c(6,5),c(6,6),c(6,7),c(6,8),c(6,9),c(6,10),c(7,1),c(7,2),c(7,3),c(7,4),c(7,5),c(7,6),c(7,7),c(7,8),c(7,9),c(7,10),c(8,1),c(8,2),c(8,3),c(8,4),c(8,5),c(8,6),c(8,7),c(8,8),c(8,9),c(8,10),c(9,1),c(9,2),c(9,3),c(9,4),c(9,5),c(9,6),c(9,7),c(9,8),c(9,9),c(9,10),c(10,1),c(10,2),c(10,3),c(10,4),c(10,5),c(10,6),c(10,7),c(10,8),c(10,9),c(10,10))
   
-  
+      while ((sum(board1[,,2]==4)!=20)&& (sum(board2[,,2]==4)!=20)){
+    
+    
+            if(turn_ind==1){
+      
+            ## PLAYER TURN ##
+      
+            ## Ask for coordinates ##
+      
+            coords_y<-readline(prompt="Insert Coordinate y: ")
+            coords_x<-readline(prompt="Insert Coordinate x: ")
+      
+            coords_x<-as.integer(coords_x)
+            coords_y<-as.integer(coords_y)
+      
+            coords=c(coords_y,coords_x)
+      
+            ## Shoot the coordinates and return result ##
+      
+            result<-shoot(board1,coords)
+      
+            message(result)
+      
+            ## if missed mark it on visible board and hidden board
+      
+            if (result=="MISS"){
+        
+                  en$board1[coords_y,coords_x,2]=4
+                  en$board1[coords_y,coords_x,1]=4
+                  board1<-en$board1
+            }
+      
+     
+            else { 
+        
+            ## if hit mark it on visible board and hidden board also perform check if ship is sunk  
+        
+            if(result=="HIT"){
+          
+                  en$board1[coords_y,coords_x,2]=2
+                  en$board1[coords_y,coords_x,1]=2
+          
+                  ## return id of hit ship
+          
+                  ship_id<-en$board1[coords_y,coords_x,3]
+              
+                  ## create temporary ship to make changes to
+
+                  ship_temp<-get(paste0("ship",ship_id),envir = en)
+              
+                  ship_temp$hits<-ship_temp$hits+1
+                  ship_temp$state<-"hit"
+                  message(ship_temp$coordinates)
+              
+                  ## check if sunk
+                
+                  if (ship_temp$hits>=length(ship_temp)) {
+                  
+                        ship_temp$state<-"sunk"
+                  
+                        ### change board to show sunk ship
+                      
+                        for (i in c(1:length(ship_temp))){
+                              message(ship_temp$coordinates[[i]][1],ship_temp$coordinates[[i]][2])
+                              en$board1[ship_temp$coordinates[[i]][1],ship_temp$coordinates[[i]][2],2]<-3
+                              en$board1[ship_temp$coordinates[[i]][1],ship_temp$coordinates[[i]][2],1]<-3
+                        
+                        }
+                  
+                    }
+              
+                  ## change ship status on en using temporary ship
+                
+                  assign(paste0("ship",ship_id),ship_temp,envir=en)
+
+                  message("HIIIIT")
+                  
+                  
+                  board1<-en$board1
+            }
+              
+            }
+        
+        
+            }
+      
+      
+      }     
+    
+    
+      if(turn_ind==(-1)){
+      
+      
+      ### AI TURN ###
+      ## fields available to shoot ##
+      
+      
+      
+      }
+  message("GAME")  
   return(0)
   
 }
-board1<-populate_board()
+
+board1<-populate_board(en)
 en$board1<-board1
 
-board2<-populate_board()
-en$board2<-board2
+board2<-populate_board(ai)
+ai$board2<-board2
 
-game(board1,board2)
 
 board1
-board2
+game(board1,board2)
+en$board1
+SHIP55<-create_ship(3,55)
+
+SHIP55$coordinates[1]
